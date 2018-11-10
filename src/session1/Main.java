bebc94d1d96faad2deb60b8a81487ec8a89e2ba8
Anton Fedorov <afedorov@yaypay.com>
2018-11-10 08:36:37 +0200

    initial commit

--- src/session1/Main.java
+++ src/session1/Main.java
@@ -1,5 +1,8 @@
 package session1;
 
+/*
+https://www.dropbox.com/s/rbbenwe2c0mou47/Java%20OOP%20%28All%20cource%29.pdf?dl=0
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("qqq");
